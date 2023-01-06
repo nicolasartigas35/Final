30eb14e5a1f72e47ee0619710a50dc5c9f9d12e3
Nicolas Artigas <nicolasjoelartigas@hotmail.com>
2023-01-06 19:15:51 -0300

    Trabajo final terminado

---
+++ Final.js
@@ -0,0 +1,11 @@
+document.getElementById('parra').addEventListener('click', function() {
+    console.log ("Mostrando referencias laborales");
+    document.getElementById("numero2").innerHTML = "3492324899 (Diego)"});
+
+document.getElementById('lasuiza').addEventListener('click', function() {
+    console.log ("Mostrando referencias laborales");
+    document.getElementById("numero1").innerHTML = "3492585258 (Victor)"});
+
+document.getElementById('pidopizza').addEventListener('click', function() {
+    console.log ("Mostrando referencias laborales");
+    document.getElementById("numero3").innerHTML = "3492624208 (Alejandro)"});
